2326c2950344d721e891b8ac17d6da1312e2bcf5
Stuart Fooks <sb.fooks@gmail.com>
2019-02-19 10:58:36 -0500

    initial index.js and file structure setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const parser = require('body-parser')
+const cors = require('cors')
+// const passport = require('./config/passport')()
+
+// const userController = require('./controllers/users')
+const postsController = require('./controllers/posts')
+
+const app = express()
+app.set('port', process.env.PORT || 8080)
+app.use(parser.json())
+app.use(cors())
+// app.use(passport.initialize())
+
+
+// app.use('/users', userController)
+app.use('/api/posts', postsController)
+
+app.listen(app.get('port'), () => {
+  console.log('Server listening on port ' + app.get('port'))
+})
