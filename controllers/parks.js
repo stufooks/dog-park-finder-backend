321e7618c352be4bc30b99c33cb904f47a6749b1
Stuart Fooks <sb.fooks@gmail.com>
2019-02-20 09:45:42 -0500

    display up to date vote value on hame page

--- controllers/parks.js
+++ controllers/parks.js
@@ -1,52 +1,54 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const Park = require('../db/Park')
+const Park = require("../db/Park");
 
-router.get('/', (req, res) => {
-  Park.find({})
-    .then(parks => {
-      res.json(parks)
-    })
-})
+router.get("/", (req, res) => {
+  Park.find({}).then(parks => {
+    res.json(parks);
+  });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
+  console.log('show method called')
+  Park.findOne({ _id: req.params.id }).then(park => {
+    console.log(park)
+    res.json(park);
+  });
+});
+
+router.post("/", (req, res) => {
+  console.log('post request called')
+  Park.create(req.body).then(park => {
+    res.json(park);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  let currentVal = 0;
   Park.findOne({ _id: req.params.id })
     .then(park => {
-      res.json(park)
-    })
-})
-
-router.post('/', (req, res) => {
-  Park.create(req.body)
-    .then(park => {
-      res.json(park)
-    })
-})
-
-router.put('/:id', (req, res) => {
-  let currentVal = 0
-  Park.findOne({ _id: req.params.id })
-    .then(park => {
-      if(req.body.upvote) {
-        currentVal = park.voteValue + 1
+      if (req.body.upvote) {
+        currentVal = park.voteValue + 1;
       } else {
-        currentVal = park.voteValue - 1
+        currentVal = park.voteValue - 1;
       }
-      })
-      .then(() => {
-        Park.findOneAndUpdate({ _id: req.params.id }, {$set: {voteValue: currentVal}}, {new: true})
-          .then(park => {
-            res.json(park)
-          })
-      })
-})
-
-router.delete('/:id', (req, res) => {
-  Park.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.send('delete complete')
     })
-})
+    .then(() => {
+      Park.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: { voteValue: currentVal } },
+        { new: true }
+      ).then(park => {
+        res.json(park);
+      });
+    });
+});
 
-module.exports = router
+router.delete("/:id", (req, res) => {
+  Park.findByIdAndDelete(req.params.id).then(() => {
+    res.send("delete complete");
+  });
+});
+
+module.exports = router;
